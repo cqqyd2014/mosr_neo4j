02342899fff30b76090fc45e5c799efed5525e59
wangli2000_cn <wangli2000_cn@126.com>
2019-06-04 17:24:09 +0800

    20190604122400

--- dataImport.py
+++ dataImport.py
@@ -1,19 +1,45 @@
-import tkinter        # 导入 Tkinter 库
-root = tkinter.Tk()                     # 创建窗口对象的背景色
+from tkinter import *        # 导入 Tkinter 库
 
 
-root.title("数据导入")
-label = tkinter.Label(root,text="数据导入工具",width=100,height=10,anchor="n")
 
-def showinfo():
-    print(entry.get())
 
-entry = tkinter.Entry(root)
-entry.pack()
-button = tkinter.Button(root, text="按钮", command=showinfo)
-button.pack()
-    
+class App:
+    def __init__(self, master):
+        
+        label_title = Label(root,text="数据导入工具")
+        label_title.pack(side=TOP)
+        #使用Frame增加一层容器
+        main_frame = Frame(master)
+        main_frame.pack(side=TOP)
+
+
+
  
-label.pack()                    # 将小部件放置到主窗口中
+        fm2 = Frame(master)
+        f_control = Frame(fm2)
+        f_control.grid(row=0, column=0)
+        f_type=Frame(f_control)
+        f_type.grid(row=0, column=0)
+        l_type=Label(f_type, text='选择数据类型').pack()
+        lb=Listbox(f_type, selectmode=SINGLE).pack()
+        for item in ["MS SQL Server","Oracle","CSV"]:
+            lb.insert(END, item)
+        f_detail=Frame(f_control)
+        f_detail.grid(row=1, column=0)
+        b_test=Button(f_control, text='测试连接')
+        b_test.grid(row=2, column=0)
+        f_data_preview = Frame(fm2)
+        f_data_preview.grid(row=1, column=0)
+        
 
-root.mainloop()   
+        fm2.pack(side=BOTTOM, fill=BOTH, expand=YES)
+
+
+
+
+root = Tk()
+root.title("数据导入")
+root.geometry('300x200')
+display = App(root)
+root.mainloop()
+
